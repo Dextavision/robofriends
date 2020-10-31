618691789e19b8a0ff25285040e2c2eb97f30087
Dextavision <dextavision@gmail.com>
2020-10-31 14:18:49 +0100

    testing container component

---
+++ src/components/MainPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./MainPage.css";
+import Header from "../components/Header";
+
+class MainPage extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  filterRobots = () => {
+    return this.props.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.props.searchField.toLowerCase());
+    });
+  };
+  render() {
+    const { onSearchChange, robots, isPending } = this.props;
+
+    return isPending ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className='tc'>
+        <Header />
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardList robots={this.filterRobots(robots)} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
