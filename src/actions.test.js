e64882f59b3e29761d5704e23a0fc3bc9b338408
Dextavision <dextavision@gmail.com>
2020-11-02 07:32:43 +0100

    add tests for actions and reducers

---
+++ src/actions.test.js
@@ -0,0 +1,12 @@
+import * as actions from "./actions";
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+it("action to search robots", () => {
+  const text = "wooo";
+  const expectedAction = {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text,
+  };
+
+  expect(actions.setSearchField(text)).toEqual(expectedAction);
+});
